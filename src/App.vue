c20c9215b98405da927dd7d256f6f876baafc273
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-31 15:49:31 +0200

    add component BaseTitle

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BaseTitle/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import BaseTitle from "./components/BaseTitle.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BaseTitle
   }
 }
 </script>
---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+export default {
+    name: 'BaseTitle'
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
